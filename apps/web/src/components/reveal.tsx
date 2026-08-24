8721af516dad4886fc1ff90f83b58ca52d862603
Walidev007 <walid_mecheri@live.fr>
2026-08-24 09:07:13 +0200

    adding space to title about me

--- apps/web/src/components/reveal.tsx
+++ apps/web/src/components/reveal.tsx
@@ -28,7 +28,7 @@ export function Reveal({ phrases, className = "", as = "div" }: RevealProps) {
       {phrases.map((phrase, index) => (
         <span
           key={index}
-          className="relative mr-1 inline-flex w-fit overflow-hidden"
+          className="relative mr-2 inline-flex w-fit overflow-hidden"
         >
           <motion.span
             className="inline-block"
